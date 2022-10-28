ab20ef8cff167817b55c592e1aef8275c57e29a8
Julia-Kalyukh <kalyuh.julia@gmail.com>
2022-10-28 03:13:35 +0300

    WordPress: Доп. шаблоны для страниц постов

--- wp-content/themes/childhood/templates/page-toys.php
+++ wp-content/themes/childhood/templates/page-toys.php
@@ -41,7 +41,7 @@ get_header();
             <div class="toys__item-descr">
               <?php the_field('toys_descr') ?>
             </div>
-            <div class="minibutton toys__trigger">Подробнее</div>
+            <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
           </div>
         </div>
 
@@ -83,7 +83,7 @@ get_header();
             <div class="toys__item-descr">
               <?php the_field('toys_descr') ?>
             </div>
-            <div class="minibutton toys__trigger">Подробнее</div>
+            <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
           </div>
         </div>
 
